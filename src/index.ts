6a515e6936731c9295fb8db62fde7c8d4960f409
Louw Swart <louw77@gmail.com>
2017-06-10 23:40:39 +1200

    Initial commit of Express User Agent Blocker

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import {NextFunction, Request, RequestHandler, Response} from 'express'
+
+/**
+ * Express middleware function to restrict access based on User Agent
+ * @param {Array<String>} userAgentToBlock
+ *          one or more (partial) User Agent strings to block
+ *          e.g. ['Baiduspider', 'SomeHorridUA']
+ * @returns {(req:Request, res:Response, next:NextFunction)=>RequestHandler}
+ */
+const euaBlocker = (userAgentToBlock: string[]): RequestHandler => {
+  const blockRegex = new RegExp(`^.*(${userAgentToBlock.join('|')}).*$`)
+  return (req: Request, res: Response, next: NextFunction) => {
+    const userAgent = (req.headers['user-agent'] || '').trim()
+
+    if (blockRegex.exec(userAgent.toLowerCase())) {
+      res.status(200).send('Nothing to see here - please move along...')
+    }
+
+    next()
+  }
+}
+
+export default euaBlocker
